f5d6eb0c2fb715c6c98781953038f2ed09e7835d
Santiago Orloff <soorloffr@unal.edu.co>
2018-04-18 19:54:37 +0000

    Functional mailer with Rails jobs

--- test/mailers/previews/welcome_mailer_preview.rb
+++ test/mailers/previews/welcome_mailer_preview.rb
@@ -3,7 +3,7 @@ class WelcomeMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/welcome_mailer/notify
   def notify
-    WelcomeMailer.notify Person.new(name: 'Sample user', email: 'sample@mail.com')
+    WelcomeMailer.notify Person.new(name: 'Usuario Prueba', email:'fakemail@gmail.com')
   end
 
 end
